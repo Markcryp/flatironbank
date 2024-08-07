295bdd696ec24d0a20bb45d6311114d110410dd6
Mark <mark.njau@student.moringaschool.com>
2024-08-07 23:11:54 +0300

    adds the searchbar

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar({setSearchTerm}) {
+    return (
+        <input 
+        type="text"
+        placeholder="Search for Transactions...."
+        onChange={(e) => setSearchTerm(e.target.value)} />
+    );
+}
+
+export default SearchBar;
